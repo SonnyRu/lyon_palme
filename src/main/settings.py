1491a08ee60af8b3e96764e1f33ca6aad62deb84
sonnyrusso <sonnyrusso@leschassagnes.net>
2023-05-03 17:01:41 +0200

    test

--- src/main/settings.py
+++ src/main/settings.py
@@ -150,10 +150,3 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_PASSWORD='wepnouydmdbfdyyu'
 RECIPIENT_ADDRESS='TheRecieverOfTheMails'
-
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
